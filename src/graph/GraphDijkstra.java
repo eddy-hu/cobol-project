bea56b613dcf0c85eec1a5c1296efed880608986
eddy-hu <eddy.hu@yahoo.com>
2019-01-06 20:58:42 -0500

    add dijstra algorithm

--- src/graph/GraphDijkstra.java
+++ src/graph/GraphDijkstra.java
@@ -2,42 +2,46 @@ package graph;
 
 public class GraphDijkstra {
 
-	private final static int MAXVEX =9;
-	private final static int MAX_WEIGHT =65535;
-	private int shortTablePath[] = new int[MAXVEX];
-	
-	
-	
-	public GraphDijkstra() {}
-	
+	private final static int MAXVEX = 9;
+	private final static int MAX_WEIGHT = 65535;
+	private int shortTablePath[] = new int[MAXVEX]; // the weight that from vertex 0 to each node
+
+	public GraphDijkstra() {
+	}
+
 	// get shortest path of graph
 	public void shortestPath(Graph graph) {
 		int min;
-		int index;
+		int index = 0;
 		boolean isGetPath[] = new boolean[MAXVEX];
-		for(int i=0; i<graph.getVertexSize();i++) {
-			shortTablePath[i]=graph.getMatrix()[0][i];
-			
+		for (int i = 0; i < graph.getVertexSize(); i++) {
+			shortTablePath[i] = graph.getMatrix()[0][i];// get 0[] vertex's weight
+
 		}
-		shortTablePath[0]=0;
-		isGetPath[0]=true;
-		for(int i=1; i<graph.getVertexSize();i++) {
-		   min = MAX_WEIGHT;
-		   for(int j=0;j<graph.getVertexSize();j++) {
-			   if(!isGetPath[j] && shortTablePath[j]<min) {
-				   index =j;
-				   min = shortTablePath[j];
-			   }
-		   }
+		shortTablePath[0] = 0;
+		isGetPath[0] = true;
+		for (int i = 1; i < graph.getVertexSize(); i++) {
+			min = MAX_WEIGHT;
+			for (int j = 0; j < graph.getVertexSize(); j++) {
+				if (!isGetPath[j] && shortTablePath[j] < min) { // get the connected node
+					index = j;
+					min = shortTablePath[j];
+				}
+
+			}
+			isGetPath[index] = true;
+			for (int k = 0; k < graph.getVertexSize(); k++) {
+				if (!isGetPath[k] && (min + graph.getMatrix()[index][k] < shortTablePath[k])) {
+					shortTablePath[k] = min + graph.getMatrix()[index][k];
+
+				}
+			}
+		}
+		
+		for(int i : shortTablePath) {
+			System.out.println(i+" "+shortTablePath[i]);
 		}
 	}
 	
-	public static void main(String[] args) {
- 
-	char a= 'y';
-    
-	System.out.println(a++);
-  
-	}
 
 }
